81574d0fbeba38cb0cea37abd67c6f686a2a956a
emersonope <emersonope@gmail.com>
2022-09-08 12:26:59 -0300

    adding date select

--- backend/src/main/java/br/com/dsmeta/services/SaleService.java
+++ backend/src/main/java/br/com/dsmeta/services/SaleService.java
@@ -3,8 +3,13 @@ package br.com.dsmeta.services;
 import br.com.dsmeta.entities.Sale;
 import br.com.dsmeta.repositories.SaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
 
 @Service
@@ -13,7 +18,15 @@ public class SaleService {
     @Autowired
     private SaleRepository repository;
 
-    public List<Sale> findSales() {
-       return repository.findAll();
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+        //pegando a data do dia da consulta
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+        //convertendo o texto string para date e verificando se a data foi passada no parametro
+        LocalDate min = minDate.equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+        return repository.findSales(min, max, pageable);
     }
 }
